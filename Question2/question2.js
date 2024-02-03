384e0e2866e3b220e4a4bb399a4d02c6a48b6d1e
Devender Swami <devenderswami53@gmail.com>
2024-02-03 15:09:02 +0530

    added question 2

---
+++ Question2/question2.js
@@ -0,0 +1,18 @@
+// Problem Statement: Create a function writeToFile(filePath, content) that 
+// takes the path to a file and user input content as input. 
+// The function should write the content to the specified file using the fs module.
+const fs = require('fs')
+function writeToFile(filePath, data) {
+    // Implementation
+    fs.writeFile(filePath,data,'utf8',(err)=>{
+        if(err){
+            console.log(`Error reading file:${err.message}`)
+         }else{
+          console.log(`Added Content in file:\n${filePath}`)
+         }
+    })
+}
+
+
+filePath='test-files/output.txt'
+writeToFile(filePath,'This is test content')
